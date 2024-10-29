3e2c600b42a0bcc651a765b07266a48fcb9b5483
장현진 <jhjhyjin@naver.com>
2024-10-29 18:58:15 +0900

    Update GameObject.h

--- Multi_Player_Programming/GameObject.h
+++ Multi_Player_Programming/GameObject.h
@@ -2,9 +2,15 @@
 
 #include <iostream>
 
+#include "OutputMemoryBitStream.h"
+
 class GameObject {
 public:
 	enum { kClassId = 'GOBJ' };
 	virtual uint32_t GetClassId() const { return kClassId; }
 	static GameObject* CreateInstance() { return new GameObject(); }
+
+	virtual void Write(OutputMemoryBitStream& inStream) {
+
+	}
 };
